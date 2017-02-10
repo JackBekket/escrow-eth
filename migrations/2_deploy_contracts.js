a3f17173fb5284e1b504a96e1413a9e9788e80e7
JackBekket <jackatwork@yandex.ru>
2017-02-10 21:28:17 +0300

    Clean

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,18 +2,9 @@ module.exports = function(deployer) {
 
 
 
-/**
-This is zeppeline deployment
-  deployer.deploy(Ownable);
-  deployer.deploy(Claimable);
-  deployer.deploy(LimitBalance);
-  if(deployer.network == 'test'){
-    deployer.deploy(SecureTargetBounty);
-    deployer.deploy(InsecureTargetBounty);
-  }
-**/
 
-deployer.deploy(EscrowSimple);
+
+//deployer.deploy(EscrowSimple);
 deployer.deploy(EscrowAdvansed);
 
 
