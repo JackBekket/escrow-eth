a66393e75ac292bb3e2d48e871cfbe796bd4b391
JackBekket <jackatwork@yandex.ru>
2017-02-15 14:22:50 +0300

    truffle3 upd2

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = function(deployer) {
 var EscrowAdvansed=artifacts.require("EscrowAdvansed.sol");
 
 //deployer.deploy(EscrowSimple);
-deployer.deploy(EscrowAdvansed,"0x0727654eb25101e5a4485852b0f014e1231604d6","10","1","1");
+deployer.deploy(EscrowAdvansed,0x0727654eb25101e5a4485852b0f014e1231604d6,10,1,1);
 
 
 };
