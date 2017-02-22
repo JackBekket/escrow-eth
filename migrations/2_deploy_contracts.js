0c23645e4de7567a2d69a8fade6be1a33fc29ae3
JackBekket <jackatwork@yandex.ru>
2017-02-22 18:58:56 +0300

    I should check names of vatiables twice

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
-
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+
+// Converted to truffle 3.1 version
+
+var EscrowAdvansed=artifacts.require("EscrowAdvansed.sol");
+
+//deployer.deploy(EscrowSimple);
+deployer.deploy(EscrowAdvansed,web3.eth.accounts[1],10,1,1);
+
+
 };
