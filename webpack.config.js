cf840c61dcf765fbc79a0407be09e8f20e3dfa51
JackBekket <jackatwork@yandex.ru>
2017-02-25 20:32:17 +0300

    flow-pack added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+var FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin');
+
 module.exports = {
   entry: './app/javascripts/app.js',
   output: {
@@ -28,7 +30,7 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['es2015'],
-          plugins: ['transform-runtime']
+          plugins: [new FlowBabelWebpackPlugin(),'transform-runtime']
         }
       }
     ]
