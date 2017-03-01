b00acdbc1cb3ca3b29b8b3f1fc247ec97f1b629d
JackBekket <jackatwork@yandex.ru>
2017-03-01 15:37:14 +0300

    semantic added and work

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var webpack = require('webpack');
 var FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin');
 
 module.exports = {
-  entry:  ['jquery','./app/javascripts/app.js'],
+  entry:  ['./app/javascripts/app.js'],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'app.js'
@@ -17,14 +17,15 @@ module.exports = {
     //  extensions: ['', '.js', '.jsx'],
     },
     node: {
-    fs: "empty",
-    module: "empty"
+    fs: "empty"
+  //  module: "empty"
    },
   plugins: [
     new webpack.ProvidePlugin({
-   $: "jquery",
-   jQuery: "jquery"
- }),
+  $: 'jquery',
+  jQuery: 'jquery'
+})
+,
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
       { from: './app/index.html', to: "index.html" }
@@ -36,19 +37,13 @@ module.exports = {
        test: /\.css$/,
        use: [ 'style-loader', 'css-loader' ]
      },
-     {
-     test: /\.scss$/,
-    use: [{
-      loader: 'style-loader',
-    }, {
-      loader: 'css-loader',
-    }, {
-      loader: 'sass-loader',
-    }]
-  }
+    { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.woff2$|\.ttf$|\.eot$|\.wav$|\.mp3$/,
+      use: ['file-loader']
+      }
   ],
     loaders: [
       { test: /\.json$/, use: 'json-loader' },
+       { test: require.resolve("jquery"), loader: "expose?$!expose?jQuery" },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         // Limiting the size of the woff fonts breaks font-awesome ONLY for the extract text plugin
@@ -56,8 +51,8 @@ module.exports = {
         loader: "url"
       },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-  { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
-      {
+    { test: /\.(ttf|eot|svg|png|jpg|jpeg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+    {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
